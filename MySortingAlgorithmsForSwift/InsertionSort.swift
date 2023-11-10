6019f57ee6f6562958ec4a856748f33524b77d04
vyatkinsen <vyatkinsen@bk.ru>
2023-11-10 18:01:02 +0300

    Добавил InsertionSort

--- MySortingAlgorithmsForSwift/InsertionSort.swift
+++ MySortingAlgorithmsForSwift/InsertionSort.swift
@@ -1,8 +1,17 @@
-//
-//  InsertionSort.swift
-//  MySortingAlgorithmsForSwift
-//
-//  Created by Арсений Вяткин on 10.11.2023.
-//
-
 import Foundation
+
+func insertionSort<T: Comparable> (_ array: [T]) -> [T] {
+    guard array.count > 1 else { return array }
+    var sortedArray = array
+    
+    for i in 1..<array.count {
+        var j = i
+        while (j > 0 && sortedArray[j - 1] > sortedArray[j]) {
+            let temp = sortedArray[j - 1]
+            sortedArray[j - 1] = sortedArray[j]
+            sortedArray[j] = temp
+            j -= 1
+        }
+    }
+    return sortedArray
+}
--- MySortingAlgorithmsForSwift/main.swift
+++ MySortingAlgorithmsForSwift/main.swift
@@ -1,5 +1,21 @@
 import Foundation
 
-let arr1 = [55, 11, 22, 44, 88]
-print(mergeSort(arr1))
+let array1 = [-27, 13, -35, 18, -21, 30, 12, -37, 24, -10]
+let array2 = [-33, 15, -28, 19, -22, 38, 14, -31, 26, -20]
+let array3 = [-40, 17, -29, 23, -25, 36, 11, -34, 31, -16]
+let array4 = [-14, 32, -26, 38, -20, 12, -37, 18, -33, 29]
+let array5 = [-22, 39, -16, 31, -24, 10, -30, 21, -35, 28]
+let array6 = [-17, 34, -15, 27, -19, 23, -40, 25, -11, 22]
+let array7 = [-31, 28, -19, 38, -14, 33, -26, 20, -35, 24]
+let array8 = [-36, 11, -34, 31, -16, 29, -23, 25, -40, 17]
+let array9 = [-10, 30, -21, 35, -28, 22, -39, 16, -31, 24]
+let array10 = [-25, 12, -37, 18, -33, 29, -22, 39, -14, 32]
 
+//let sortedArr = mergeSort(array1)
+//print(sortedArr)
+
+//let sortedArr = bubbleSort(array2)
+//print(sortedArr)
+
+//let sortedArr = insertionSort(array2)
+//print(sortedArr)
